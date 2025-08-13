3a46c3f059cb1bfaa0085abb11cc5cec733f6dd2
lucasmattoso-dev <lucasnmattoso@gmail.com>
2025-08-13 02:25:57 -0300

    terceira commit

--- src/script.js
+++ src/script.js
@@ -1,30 +1,28 @@
-// Variáveis para nome e XP
-let nomeHeroi = "Luffy";
-let xp = 8500; 
+// 1) Variáveis: nome e XP do herói
+let nome = "Luffy"
+let xp = 8640
 
-// Variável para armazenar o nível
-let nivel = "";
+// 2) Variável que vai guardar o nível do herói
+let nivel = ""
 
-// Estrutura de decisão para classificar o herói
+// 3) Estrutura de decisão: define o nível conforme o XP
 if (xp < 1000) {
-    nivel = "Ferro";
+    nivel = "Ferro"
 } else if (xp >= 1001 && xp <= 2000) {
-    nivel = "Bronze";
-} else if (xp >= 2001 && xp <= 5000) {
-    nivel = "Prata";
+    nivel = "Bronze"
+}  else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Patra"
 } else if (xp >= 5001 && xp <= 7000) {
-    nivel = "Ouro";
+    nivel = "Ouro"
 } else if (xp >= 7001 && xp <= 8000) {
-    nivel = "Platina";
+    nivel = "Platina"
 } else if (xp >= 8001 && xp <= 9000) {
-    nivel = "Ascendente";
+    nivel = "Ascendente"
 } else if (xp >= 9001 && xp <= 10000) {
-    nivel = "Imortal";
-} else if (xp >= 10001) {
-    nivel = "Radiante";
+    nivel = "Imortal"
 } else {
-    nivel = "Nível desconhecido"; // caso caia fora das condições
-}
+  nivel = "Radiante";
+} 
 
-// Saída final
-console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+// 4) Saída final 
+console.log (`O Herói de nome ${nome} está no nível de ${nivel}`)
